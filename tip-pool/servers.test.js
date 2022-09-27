67fafc169417ffe74b5fde33c4bc15e37cd3b93a
Matt Campbell <cmampbell@gmail.com>
2022-09-27 11:00:27 -0400

    added testing for sumbitServerInfo and updateServerTable

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -12,6 +12,36 @@ describe("Servers test (with setup and tear-down)", function() {
   });
 
   afterEach(function() {
-    // teardown logic
+    delete allServers['server' + serverId];
+    for(let child of serverTbody.children){
+      child.remove();
+    }
+    serverId = 0;
   });
 });
+
+//write a test for updateServerTable()
+describe("Servers test with updating server table", function(){
+  let testServers = ['Matt', 'Caroline', 'Jose', 'Talcum'];
+  beforeEach(function (){
+    //check if it adds all servers from an existing object
+    for(let serverName of testServers){
+      serverId++;
+      allServers['server' + serverId] = { serverName };
+    }
+  })
+
+  it('should update the table length to match the length of the array', function() {
+    updateServerTable()
+
+    expect(Object.keys(allServers).length).toEqual(testServers.length);
+  })
+
+  afterEach(function(){
+    for(let key in allServers){
+      delete allServers[key];
+    }
+    serverId = 0;
+    updateServerTable();
+  })
+})
